8d2b993272bc769e08a4754ed8596344f98a8530
sarthaksg1997 <sarthakg662@gmail.com>
2021-10-08 17:45:46 +0530

    changed in database.js

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const Connection = async (URL) => {
      
     try {
-        await mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify: true });
+        await mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
         console.log('Database connected succesfully');
     } catch (error) {
         console.log(error.message);
--- server.js
+++ server.js
@@ -50,6 +50,6 @@ paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID,
 paytmParams['ORDER_ID'] = uuid(),
 paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID,
 paytmParams['TXN_AMOUNT'] = '100',
-paytmParams['CALLBACK_URL'] = 'http://localhost:8000/callback'
+paytmParams['CALLBACK_URL'] = '/callback'
 paytmParams['EMAIL'] = 'sarthakg662@gmail.com'
 paytmParams['MOBILE_NO'] = '9148497208'
